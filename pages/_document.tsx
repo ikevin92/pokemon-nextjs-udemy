5ea0e574848804306556947526778ec00068757c
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-03 10:32:34 -0500

    update document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,20 +9,7 @@ import { CssBaseline } from '@nextui-org/react';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
-        const originalRenderPage = ctx.renderPage;
-
-        // Run the React rendering logic synchronously
-        ctx.renderPage = () =>
-            originalRenderPage({
-                // Useful for wrapping the whole react tree
-                enhanceApp: (App) => App,
-                // Useful for wrapping in a per-page basis
-                enhanceComponent: (Component) => Component,
-            });
-
-        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
         const initialProps = await Document.getInitialProps(ctx);
-
         return {
             ...initialProps,
             styles: <>{initialProps.styles}</>,
@@ -31,7 +18,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-            <Html>
+            <Html lang='en'>
                 <Head>{CssBaseline.flush()}</Head>
                 <body>
                     <Main />
